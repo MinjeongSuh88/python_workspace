33da9fb61a5b6217c7249e5829ab32c39d60910c
MinjeongSuh88 <69196506+MinjeongSuh88@users.noreply.github.com>
2020-08-05 17:17:51 +0900

    20200805오후

--- 20200805/memo.py
+++ 20200805/memo.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 class MyApp(QMainWindow):
     def __init__(self):
@@ -15,30 +16,87 @@ class MyApp(QMainWindow):
         self.setWindowIcon(QIcon('./img/memo.jpeg'))
         self.show()
 
-        openFile = QAction(QIcon('./img/instagram.png'),'열기',self)
+        # 메뉴바 만들기 - 새파일, 열기, 저장, 종료
+        openFile = QAction(QIcon('./img/instagram.png'),'열기',self) # 메뉴바-File 아래에 열기 객체 만듦
         openFile.setShortcut('Ctrl+O')
         openFile.setStatusTip('새 파일 열기')
+        openFile.triggered.connect(self.showDialog) # 방아쇠
 
-        newFile = QAction(QIcon('./img/melong.png'),'새파일',self)
+        newFile = QAction(QIcon('./img/melong.png'),'새파일',self) # 메뉴바-File 아래에 새파일 객체 만듦
         newFile.setShortcut('Ctrl+N')
-        newFile.setStatusTip('뭐')
+        newFile.setStatusTip('새 파일 만들기')
+        newFile.triggered.connect(self.newFile) # 방아쇠
         
-        saveFile = QAction(QIcon('.img/somson.png'),'저장',self)
+        saveFile = QAction(QIcon('.img/somson.png'),'저장',self) # 메뉴바-File 아래에 저장 객체 만듦
         saveFile.setShortcut('Ctrl+S')
-        saveFile.setStatusTip('')
+        saveFile.setStatusTip('저장하기')
+        saveFile.triggered.connect(self.saveDialog) # 방아쇠
 
-        exitFile = QAction(QIcon('.img/somson/png'),'종료',self)
+        exitFile = QAction(QIcon('.img/somson/png'),'종료',self) # 메뉴바-File 아래에 종료 객체 만듦
         exitFile.setShortcut('Ctrl+X')
-        exitFile.setStatusTip('')
+        exitFile.setStatusTip('종료하기')
+        exitFile.triggered.connect(QCoreApplication.instance().quit) # 창 종료
 
-        # 메뉴바 만들기 - 새파일, 열기, 저장, 종료
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False) # 전통적인 메뉴바 안 쓴다
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(openFile)
+        fileMenu = menubar.addMenu('&File') # 메뉴바 만듦
+        fileMenu.addAction(openFile) # 만들어놓은 열기 객체 추가
         fileMenu.addAction(newFile)
         fileMenu.addAction(saveFile)
         fileMenu.addAction(exitFile)
+
+        # 메뉴바-서식 만들기
+        fontMenu = QAction(QIcon('./img/font.png'),'폰트',self)
+        fontMenu.setShortcut('Ctrl+F')
+        fontMenu.setStatusTip('글꼴')
+        fontMenu.triggered.connect(self.changeFont) 
+
+        formMenu = menubar.addMenu('&서식')
+        formMenu.addAction(fontMenu)
+
+    def changeFont(self):
+        # print('글꼴 누름')
+        font, ok = QFontDialog.getFont() # 만들어 놓은 함수 사용하여 글씨체와 변경 여부 가져옴
+        if ok:
+            self.textEdit.setFont(font)
+
+    def showDialog(self):
+        fname = QFileDialog.getOpenFileName(self,'open file','./') # open file :다이얼로그 창 이름
+        print('show dialog 함수 호출됨')
+        print(fname)
+        
+        if fname[0]:
+            f = open(fname[0],'r',encoding='utf-8')
+            with f:
+                data = f.read()
+                self.textEdit.setText(data)
+                name = fname[0].split('/')
+                print(name[-1].split('.')[0])
+                self.setWindowTitle(name[-1].split('.')[0] + ' Windows 메모장')
+
+    def saveDialog(self):
+        # 저장창을 띄우기
+        file = QFileDialog.getSaveFileName(self,'저장해봐~~','./') # 저장하는 창 다이얼로그
+        print(file)
+        # file I/O
+        with open(file[0],'w') as f:
+            f.write(self.textEdit.toPlainText()) # toPlainText() :textEdit객체에서 문자열만 뽑아냄
+
+
+    def newFile(self):
+        # 기존에 있는 텍스트를 초기화
+        print('new dialog 함수 호출됨')
+        # textEdit에 내용이 있는지 판단
+        # 내용이 존재한다면
+        if len(self.textEdit.toPlainText()) != 0:
+        # 저장할 것인지를 물어보기, 저장한 후에 지우기
+            rep = QMessageBox.question(self,'메모장','변경 내용을 제목 없음에 저장하시겠습니까?',QMessageBox.Yes|QMessageBox.No|QMessageBox.Cancel,QMessageBox.Yes)
+            if rep == QMessageBox.Yes:
+                self.saveDialog()
+        # 내용이 없으면 그냥 지우기
+        self.textEdit.setText('')
+
+        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ 20200805/paint.py
@@ -0,0 +1,102 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(100,100,1000,800)
+        # 전체 폼
+        frmbox = QHBoxLayout()
+        self.setLayout(frmbox) # 현재창에 frmbox 레이아웃 적용
+
+        # 좌측 레이아웃
+        leftbox = QVBoxLayout()
+        rightbox = QVBoxLayout()
+
+        # 좌측 레이아웃
+        gb = QGroupBox('타입')
+        leftbox.addWidget(gb) # leftbox에 gb 위젯 추가
+
+        box1 = QVBoxLayout()
+        gb.setLayout(box1) # 그룹박스에 수직박스레이아웃 적용
+
+        box1.addWidget(QRadioButton('선',self)) # 그룹      
+        box1.addWidget(QRadioButton('곡선',self)) # 그룹      
+        box1.addWidget(QRadioButton('사각형',self)) # 그룹      
+        box1.addWidget(QRadioButton('타원',self)) # 그룹      
+
+        # 그룹박스2
+        gb2 = QGroupBox('Pen setting')
+        leftbox.addWidget(gb2)
+
+        grid = QGridLayout() 
+        gb2.setLayout(grid) # gb2에 그리드 형식 적용
+
+        label = QLabel('선굵기')
+        grid.addWidget(label,0,0) # 0행 0열에 레이블 위젯 추가
+
+        combo = QComboBox()
+        grid.addWidget(combo,0,1)
+        for i in range(1,21):
+            combo.addItem(str(i))
+
+        label2 = QLabel('선색')
+        grid.addWidget(label2,1,0)
+
+        # 펜 색상
+        self.pencolor = QColor(0,0,0) # self를 붙여서 전역변수 화
+        self.penbtn = QPushButton()
+        self.penbtn.setStyleSheet('background-color:rgb(0,0,0)') # css형식으로 스타일 지정
+
+        grid.addWidget(self.penbtn,1,1)
+
+        # 그룹 박스 3
+        # 부 설정
+        gb3 = QGroupBox('부 설정')
+        leftbox.addWidget(gb3)
+
+        hbox = QHBoxLayout()
+        gb3.setLayout(hbox)
+
+        label3 = QLabel('부 색상')
+        hbox.addWidget(label3)
+
+        self.brushcolor = QColor(255,255,255)
+        self.brushbtn = QPushButton()
+        self.brushbtn.setStyleSheet('background-color:rgb(255,255,255)')
+        hbox.addWidget(self.brushbtn)
+
+        # 우측 레이아웃 박스에 그래픽 뷰 추가
+        self.view = CGView(self)
+        rightbox.addWidget(self.view)
+
+        frmbox.addLayout(leftbox)
+        frmbox.addLayout(rightbox)
+
+        self.show()
+
+# QGraphics는 시각적 객체의 복잡한 장면을 쉽게 처리
+# 할 수 있는 프레임 워크로 구성하는데 사용할 수 있다.
+
+# QGraphicsView,
+
+class CGView(QGraphicsView):
+    def __init__(self,parent):
+        super().__init__(parent)
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)
+
+        self.item = []
+        self.start = QPointF() # 시작 위치
+        self.end = QPointF()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    m = MyApp()
+    sys.exit(app.exec_())
